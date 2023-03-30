8eb453b4618b33949a8904a3642630b0b0afc57f
GKatanski <grzegorz.katanski@gmail.com>
2023-03-30 16:27:24 +0200

    report 1 version

--- main.py
+++ main.py
@@ -4,13 +4,28 @@ def main():
     #print(text)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-    num_words_report = get_words(text)
-    print(f"{num_words_report}")
+    #num_words_report = get_words(text)
+    #print(f"{num_words_report}")
     #print(sorted(num_words_report.items(), key = lambda items:items[1], reverse = True))
-    num_of_letters = get_num_of_letters(text)
-    print(num_of_letters)
+    #num_of_letters = get_num_of_letters(text)
+    #print(num_of_letters)
+    report_of_the_book(text,book_path)
+    
+def report_of_the_book(text,path):
+    print(f"--- the begining of the report of the book: {str(path)} ---")
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document\n\n")
+    num_of_letters =list(get_num_of_letters(text).items())
+    num_of_letters.sort()
+    num_of_letters.sort(key = lambda a:a[1], reverse=True)
+    for l in num_of_letters:
+        if l[0].isalpha():
+            print(f"The '{l[0]}' charakter was found {l[1]} times")
+    print("\n --- End of the report ---")
+    
 
-    #print(sorted(num_words_report.items(), key = lambda items:items[1], reverse = True))
+    return
+    
 
 def get_num_words(text):
     words = text.split()
